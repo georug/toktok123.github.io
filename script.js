b9cc7a88d4f14a4b9bc119624b414e9d0bd9691c
georug <georug01@gmail.com>
2025-01-14 22:05:45 +0800

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Script sederhana untuk menambah interaksi jika diperlukan
-document.querySelector('.hero button').addEventListener('click', function() {
-    alert("Anda akan belajar lebih lanjut tentang SolMeme!");
-});
